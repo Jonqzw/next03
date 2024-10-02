a645445bf9d31c4cd7951852ccd29eb4cb2ac0d8
Jonqzw <vidarschaal@gmail.com>
2024-10-02 13:44:12 +0200

    IDK second commit

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -3,16 +3,35 @@ import getAllUseres from "@/lib/getAllUseres";
 import Link from "next/link";
 
 export const metadata: Metadata = {
-    title: 'Users'
-}
+  title: "Users",
+};
 
-import React from 'react'
+import React from "react";
 
 export default async function UsersPage() {
-    const userData: Promise<User[]> = getAllUseres();
+  const userData: Promise<User[]> = getAllUseres();
 
-    const users = await userData;
-  return (
-    <div>page</div>
-  )
+  const users = await userData;
+
+  console.log('Hello')
+
+  const content = (
+    <section>
+      <h2>
+        <Link href="/">Back to Home</Link>
+      </h2>
+      <br />
+      {users.map((user) => {
+        return (
+          <>
+            <p key={user.id}>
+              <Link href={"/users/${user.id"}>{user.name}</Link>
+            </p>
+            <br />
+          </>
+        );
+      })}
+    </section>
+  );
+  return content;
 }
